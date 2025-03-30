605b0e412093ba3892fc9f7c3f6254f938a5d313
Your Name <you@example.com>
2025-03-30 21:47:18 +0200

    Update GitHub Pages configuration and workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { writeFileSync } from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'create-nojekyll',
+      closeBundle() {
+        writeFileSync('./.nojekyll', '')
+        writeFileSync('./dist/.nojekyll', '')
+      }
+    }
+  ],
   base: '/songbook/',
 })
