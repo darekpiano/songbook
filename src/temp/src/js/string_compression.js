f4364b7c7014a1faf4c463959030966ecb02faa7
Your Name <you@example.com>
2025-03-30 23:15:07 +0200

    Remove temporary files and directories related to the chord sheet editor and viewer

--- src/temp/src/js/string_compression.js
+++
@@ -1,9 +0,0 @@
-import LZString from 'lz-string';
-
-export function compress(input) {
-  return LZString.compressToEncodedURIComponent(input);
-}
-
-export function decompress(input) {
-  return LZString.decompressFromEncodedURIComponent(input);
-}
