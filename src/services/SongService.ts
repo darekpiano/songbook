94482f480c7a8674dcb4fc05b1671a6d3190e9a3
Your Name <you@example.com>
2025-03-31 08:56:14 +0200

    Enhance song formatting logic in SongService and update CSS styles for improved text and HTML display

--- src/services/SongService.ts
+++ src/services/SongService.ts
@@ -1,12 +1,14 @@
-import { ChordProParser, Song, HtmlDivFormatter, Metadata } from 'chordsheetjs';
+import { ChordProParser, Song, HtmlDivFormatter, TextFormatter, Metadata } from 'chordsheetjs';
 
 export class SongService {
   private parser: ChordProParser;
   private htmlFormatter: HtmlDivFormatter;
+  private textFormatter: TextFormatter;
 
   constructor() {
     this.parser = new ChordProParser();
     this.htmlFormatter = new HtmlDivFormatter();
+    this.textFormatter = new TextFormatter();
   }
 
   async loadSong(filename: string): Promise<Song | null> {
@@ -23,75 +25,69 @@ export class SongService {
   }
 
   formatSong(song: Song, showChords: boolean = true): string {
-    const formattedSong = this.htmlFormatter.format(song);
-    
-    if (!showChords) {
-      const tempDiv = document.createElement('div');
-      tempDiv.innerHTML = formattedSong;
-      const chordElements = tempDiv.getElementsByClassName('chord');
-      Array.from(chordElements).forEach((chord: Element) => {
-        const chordElement = chord as HTMLElement;
-        chordElement.style.visibility = 'hidden';
-        chordElement.style.height = '0';
-        chordElement.style.width = '0';
-        chordElement.style.position = 'absolute';
-        chordElement.style.pointerEvents = 'none';
-      });
-      return `<div class="songContent" data-mode="text">${tempDiv.innerHTML}</div>`;
+    let formattedContent: string;
+    let dataMode: string;
+
+    if (showChords) {
+      formattedContent = this.htmlFormatter.format(song);
+      dataMode = "html";
+    } else {
+      formattedContent = this.textFormatter.format(song);
+      dataMode = "text";
     }
 
     const baseStyles = `
       .songContent {
         white-space: pre;
         width: 100%;
+        font-size: 1.1em;
+        line-height: 1.6;
       }
+      
       .songContent[data-mode="text"] {
         font-family: monospace;
         font-size: 14px;
         line-height: 1.5;
       }
-      .songContent[data-mode="text"] .chord {
-        visibility: hidden;
-        height: 0;
-        width: 0;
-        position: absolute;
-        pointer-events: none;
-      }
+      
       .songContent[data-mode="html"] {
         font-family: inherit;
       }
+      
       .songContent .chord {
         font-weight: 600;
         color: var(--primary-color);
         font-family: monospace;
       }
+      
       .songContent .chord:not(:last-child) {
         padding-right: 1em;
       }
+      
       .songContent .empty-line {
         height: 1em;
       }
+      
       .songContent .paragraph {
         margin-bottom: 1em;
-        width: 100%;
       }
-      .songContent .lyrics {
-        display: block;
-        width: 100%;
-        padding-right: 2em;
-      }
+      
       .songContent .chord:after,
       .songContent .lyrics:after {
         content: '\\200b';
       }
+      
       .songContent .row {
-        display: block;
-        width: 100%;
+        display: flex;
         line-height: 150%;
-        min-height: 1.5em;
       }
     `;
-    return `<style>${baseStyles}</style><div class="songContent" data-mode="html">${formattedSong}</div>`;
+
+    if (dataMode === 'text') {
+      return `<style>${baseStyles}</style><pre class="songContent" data-mode="${dataMode}">${formattedContent}</pre>`;
+    } else {
+      return `<style>${baseStyles}</style><div class="songContent" data-mode="${dataMode}">${formattedContent}</div>`;
+    }
   }
 
   transposeUp(song: Song): Song {
